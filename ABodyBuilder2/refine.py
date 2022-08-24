32029354c3fd3958daa51d6805a7205e13b7ac2e
Brennan Abanades <spamspambrennan@gmail.com>
2022-08-24 15:53:32 +0100

    Updated refinement to hopefully solve stereo problem

--- ABodyBuilder2/refine.py
+++ ABodyBuilder2/refine.py
@@ -1,6 +1,6 @@
 import pdbfixer
 import numpy as np
-from simtk.openmm import app, LangevinIntegrator, CustomExternalForce, CustomTorsionForce, OpenMMException
+from simtk.openmm import app, LangevinIntegrator, CustomExternalForce, CustomTorsionForce, HarmonicBondForce, OpenMMException
 from simtk import unit
 
 ENERGY = unit.kilocalories_per_mole
@@ -10,45 +10,51 @@ spring_unit = ENERGY / (LENGTH ** 2)
 
 def refine(input_file, output_file, n=5):
     k1s = [2.5,1,0.5,0.25,0.1]
-    k2s = [1,2.5,5,10,25]
+    k2s = [2.5,5,7.5,15,25]
 
     fixer = pdbfixer.PDBFixer(input_file)
 
     fixer.findMissingResidues()
     fixer.findMissingAtoms()
     fixer.addMissingAtoms()
-
-    topology, positions = refine_once(fixer.topology, fixer.positions, k1=k1s[0], k2 = k2s[0])
+    
+    k1 = k1s[0]
+    k2 = -1 if cis_check(fixer.topology, fixer.positions) else k2s[0]
+    topology, positions = refine_once(fixer.topology, fixer.positions, k1=k1, k2=k2)
 
     for i in range(1,n):
-        if not (bond_check(topology, positions) or cis_check(topology, positions)):
-            print("all failed")
-            topology, positions = refine_once(fixer.topology, fixer.positions, k1=k1s[i], k2 = k2s[i])
-        elif not bond_check(topology, positions):
-            print("bonds failed")
-            topology, positions = refine_once(fixer.topology, fixer.positions, k1=k1s[i], k2 = k2s[0])
-        elif not cis_check(topology, positions):
+        acceptable_bonds, trans_peptide_bonds = bond_check(topology, positions), cis_check(topology, positions)
+        if not acceptable_bonds:
+            print("Bonds failed")
+            k1 = k1s[i]
+        if not trans_peptide_bonds:
             print("CIS failed")
-            topology, positions = refine_once(fixer.topology, fixer.positions, k1=k1s[0], k2 = k2s[i])
-        else:
-            print("all good")
+            k2 = k2s[i]
+        if not stereo_check(topology, positions):
+            print("Stereo failed")
+        if acceptable_bonds and trans_peptide_bonds and stereo_check(topology, positions):
             break
-    
-    if not stereo_check(topology, positions): 
-        print("D-amino acids in model")
+        else:
+            try:
+                topology, positions = refine_once(fixer.topology, fixer.positions, k1=k1, k2 = k2)
+            except OpenMMException:
+                continue
+
+    if not stereo_check(topology, positions):
+        print("Stereo fail")
 
     with open(output_file, "w") as out_handle:
         app.PDBFile.writeFile(topology, positions, out_handle, keepIds=True)
 
 
-def refine_once(topology, positions, k1=1.0, k2=5.0):
+def refine_once(topology, positions, k1=2.5, k2=2.5):
 
     # Using amber14 recommended protein force field
     forcefield = app.ForceField("amber14/protein.ff14SB.xml")
 
     # Fill in the gaps with OpenMM Modeller
     modeller = app.Modeller(topology, positions)
-    modeller.addHydrogens(forcefield)
+    modeller.addHydrogens()
 
     # Set up force field
     system = forcefield.createSystem(modeller.topology)
@@ -64,17 +70,19 @@ def refine_once(topology, positions, k1=1.0, k2=5.0):
             if atom.name in ["CA", "CB", "N", "C"]:
                 force.addParticle(atom.index, modeller.positions[atom.index])
     
-    cis_force = CustomTorsionForce("10*k2*(1+cos(theta))^2")
-    cis_force.addGlobalParameter("k2", k2 * ENERGY)
-
-    for chain in modeller.topology.chains():
-        residues = [[atom.index for atom in res.atoms() if atom.name in ["N", "CA", "C"]] for res in chain.residues()]
-        for i,resi in enumerate(residues[:-1]):
-            n_resi = residues[i+1]
-            cis_force.addTorsion(resi[1], resi[2], n_resi[0], n_resi[1])
-    
     system.addForce(force)
-    system.addForce(cis_force)
+
+    if k2 > 0.0:
+        cis_force = CustomTorsionForce("10*k2*(1+cos(theta))^2")
+        cis_force.addGlobalParameter("k2", k2 * ENERGY)
+
+        for chain in modeller.topology.chains():
+            residues = [{atom.name:atom.index for atom in res.atoms() if atom.name in ["N", "CA", "C"]} for res in chain.residues()]
+            for i,resi in enumerate(residues[:-1]):
+                n_resi = residues[i+1]
+                cis_force.addTorsion(resi["CA"], resi["C"], n_resi["N"], n_resi["CA"])
+        
+        system.addForce(cis_force)
 
     # Set up integrator
     integrator = LangevinIntegrator(0, 0.01, 0.0)
@@ -91,9 +99,9 @@ def refine_once(topology, positions, k1=1.0, k2=5.0):
 
 def bond_check(topology, positions):
     for chain in topology.chains():
-        residues = [[atom.index for atom in res.atoms() if atom.name in ["N", "C"]] for res in chain.residues()]
+        residues = [{atom.name:atom.index for atom in res.atoms() if atom.name in ["N", "C"]} for res in chain.residues()]
         for i in range(len(residues)-1):
-            v = np.linalg.norm(positions[residues[i][1]] -  positions[residues[i+1][0]])
+            v = np.linalg.norm(positions[residues[i]["C"]] -  positions[residues[i+1]["N"]])
             if abs(v - 1.329*LENGTH) > 0.1*LENGTH:
                 return False
     return True
@@ -114,9 +122,9 @@ def cos_of_torsion(p0,p1,p2,p3):
 
 def cis_check(topology, positions):
     for chain in topology.chains():
-        residues = [[atom.index for atom in res.atoms() if atom.name in ["N", "CA", "C"]] for res in chain.residues()]
+        residues = [{atom.name:atom.index for atom in res.atoms() if atom.name in ["N", "CA", "C"]} for res in chain.residues()]
         for i in range(len(residues)-1):
-            p0,p1,p2,p3 = positions[residues[i][1]],positions[residues[i][2]],positions[residues[i+1][0]],positions[residues[i+1][1]]
+            p0,p1,p2,p3 = positions[residues[i]["CA"]],positions[residues[i]["C"]],positions[residues[i+1]["N"]],positions[residues[i+1]["CA"]]
             if cos_of_torsion(p0,p1,p2,p3) > 0:
                 return False
     return True
@@ -124,11 +132,12 @@ def cis_check(topology, positions):
 
 def stereo_check(topology, positions):
     for residue in topology.residues():
-        atom_indices = [atom.index for atom in residue.atoms() if atom.name in ["CA", "N", "C", "CB"]]
-        vectors = [positions[i] - positions[atom_indices[0]] for i in atom_indices[1:]]
         if residue.name == "GLY":
             continue
 
-        if np.dot(np.cross(vectors[0], vectors[1]), vectors[2]) > .0*LENGTH**3:
+        atom_indices = {atom.name:atom.index for atom in residue.atoms() if atom.name in ["N", "CA", "C", "CB"]}
+        vectors = [positions[atom_indices[i]] - positions[atom_indices["CA"]] for i in ["N", "C", "CB"]]
+
+        if np.dot(np.cross(vectors[0], vectors[1]), vectors[2]) < .0*LENGTH**3:
             return False
     return True
