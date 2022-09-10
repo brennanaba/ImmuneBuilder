6414b1c42f9bf31d08a7147d77548d5524bddfc1
Brennan Abanades <spamspambrennan@gmail.com>
2022-09-10 18:14:18 +0100

    Download now happens when you initialise the class

--- ImmuneBuilder/ABodyBuilder2.py
+++ ImmuneBuilder/ABodyBuilder2.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import os
 from ImmuneBuilder.models import StructureModule
-from ImmuneBuilder.util import get_encoding, to_pdb, find_alignment_transform
+from ImmuneBuilder.util import get_encoding, to_pdb, find_alignment_transform, download_file
 from ImmuneBuilder.refine import refine
 
 torch.set_default_tensor_type(torch.DoubleTensor)
@@ -13,6 +13,11 @@ embed_dim = {
     "antibody_model_4":256
 }
 
+model_urls = {
+    "antibody_model_2": "https://dl.dropbox.com/s/hez39qf0kyncscw/antibody_model_2?dl=1",
+    "antibody_model_3": "https://dl.dropbox.com/s/tsk4zw5xsj0a7pk/antibody_model_3?dl=1",
+    "antibody_model_4": "https://dl.dropbox.com/s/quww8407ae7f076/antibody_model_4?dl=1",
+}
 
 class Antibody:
     def __init__(self, sequence_dict, predictions):
@@ -61,7 +66,17 @@ class ABodyBuilder2:
             model_file = f"antibody_model_{id}"
             model = StructureModule(rel_pos_dim=64, embed_dim=embed_dim[model_file]).to(self.device)
             weights_path = os.path.join(current_directory, "trained_model", model_file)
-            model.load_state_dict(torch.load(weights_path, map_location=torch.device(self.device)))
+
+            try:
+                if not os.path.exists(weights_path):
+                    print(f"Attempting to download weights for {model_file}...")
+                    download_file(model_urls[model_file], weights_path)
+
+                model.load_state_dict(torch.load(weights_path, map_location=torch.device(self.device)))
+            except Exception:
+                print(f"{model_file} not downloaded or file corrupted.")
+                continue
+
             model.eval()
 
             self.models[model_file] = model
--- ImmuneBuilder/util.py
+++ ImmuneBuilder/util.py
@@ -1,11 +1,22 @@
 import numpy as np
 import torch
 from ImmuneBuilder.constants import res_to_num, atom_types, residue_atoms, restype_1to3
-from ImmuneBuilder.rigids import Rigid
+import requests
+
+
+def download_file(url, filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, 'wb+') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+    return filename
+
 
 def get_one_hot(targets, nb_classes=21):
     res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
     return res.reshape(list(targets.shape) + [nb_classes])
+
 
 def get_encoding(sequence_dict, chain_ids="HL"):
     
