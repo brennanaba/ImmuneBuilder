e6d2543d3b1d40432581b05f959db2fc7c9b3e83
Brennan Abanades <spamspambrennan@gmail.com>
2022-09-10 17:58:05 +0100

    Add code to deal with multiple models

--- ImmuneBuilder/ABodyBuilder2.py
+++ ImmuneBuilder/ABodyBuilder2.py
@@ -1,51 +1,80 @@
 import torch
+import numpy as np
 import os
 from ImmuneBuilder.models import StructureModule
-from ImmuneBuilder.util import get_encoding, to_pdb
+from ImmuneBuilder.util import get_encoding, to_pdb, find_alignment_transform
 from ImmuneBuilder.refine import refine
 
 torch.set_default_tensor_type(torch.DoubleTensor)
+embed_dim = {
+    "antibody_model_1":128,
+    "antibody_model_2":256,
+    "antibody_model_3":256,
+    "antibody_model_4":256
+}
+
 
 class Antibody:
-    def __init__(self, sequence_dict, output):
-        self.atoms, self.encodings = output
+    def __init__(self, sequence_dict, predictions):
         self.sequence_dict = sequence_dict
+        self.atoms = [x[0] for x in predictions]
+        self.encodings = [x[1] for x in predictions]
 
-    def save_unrefined(self, filename):
-        unrefined = to_pdb(self.sequence_dict, self.atoms)
+        with torch.no_grad():
+            traces = torch.stack([x[:,0] for x in self.atoms])
+            self.R,self.t = find_alignment_transform(traces)
+            self.aligned_traces = (traces-self.t) @ self.R
+            self.error_estimates = (self.aligned_traces - self.aligned_traces.mean(0)).square().sum(-1)
+            self.ranking = [x.item() for x in self.error_estimates.mean(-1).argsort()]
+        
+
+    def save_unrefined(self, filename, index=0):
+        atoms = (self.atoms[index] - self.t[index]) @ self.R[index]
+        unrefined = to_pdb(self.sequence_dict, atoms)
 
         with open(filename, "w+") as file:
             file.write(unrefined)
 
 
     def save(self, filename):
-        try:
-            name, filetype = filename.split(".")
-        except ValueError:
-            name = "unrefined"
-            filetype = "pdb"
+        for i in range(len(self.atoms)):
+            try:
+                name, filetype = filename.split(".")
+            except ValueError:
+                name = "prediction"
+                filetype = "pdb"
 
-        unrefined_filename = name + "_unrefined." + filetype
-        self.save_unrefined(unrefined_filename)
-        refine(unrefined_filename, filename)
+            unrefined_filename = name + f"_rank{self.ranking.index(i)}_unrefined." + filetype
+            self.save_unrefined(unrefined_filename, index=i)
+
+        np.save(name + "_error_estimates", self.error_estimates.mean(0).numpy())
+        refine(name + f"_rank{0}_unrefined." + filetype, filename)
 
 
 class ABodyBuilder2:
-    def __init__(self):
+    def __init__(self, model_ids = [1,2,3,4]):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         current_directory = os.path.dirname(os.path.realpath(__file__))
 
-        self.model = StructureModule(rel_pos_dim=64, embed_dim=128).to(self.device)
-        self.model.eval()
-        path = os.path.join(current_directory, "trained_model", "antibody_model_1")
-        self.model.load_state_dict(torch.load(path, map_location=torch.device(self.device)))
+        self.models = {}
+        for id in model_ids:
+            model_file = f"antibody_model_{id}"
+            model = StructureModule(rel_pos_dim=64, embed_dim=embed_dim[model_file]).to(self.device)
+            weights_path = os.path.join(current_directory, "trained_model", model_file)
+            model.load_state_dict(torch.load(weights_path, map_location=torch.device(self.device)))
+            model.eval()
+
+            self.models[model_file] = model
 
 
     def predict(self, sequence_dict):
         with torch.no_grad():
             encoding = torch.tensor(get_encoding(sequence_dict), device = self.device)
             full_seq = "".join(sequence_dict.values())
+            outputs = []
 
-            output = self.model(encoding, full_seq)
+            for model_file in self.models:
+                pred = self.models[model_file](encoding, full_seq)
+                outputs.append(pred)
 
-        return Antibody(sequence_dict, output)
+        return Antibody(sequence_dict, outputs)
--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
 from setuptools import setup, find_packages
+import requests
+import os
+
+
+def download_file(url, filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+    return filename
+
+model_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "trained_model")
+
+model_urls = {
+    "antibody_model_2": "https://dl.dropbox.com/s/hez39qf0kyncscw/antibody_model_2?dl=1",
+    "antibody_model_3": "https://dl.dropbox.com/s/tsk4zw5xsj0a7pk/antibody_model_3?dl=1",
+    "antibody_model_4": "https://dl.dropbox.com/s/quww8407ae7f076/antibody_model_4?dl=1",
+}
+
+for file in model_urls:
+    download_file(model_urls[file], os.path.join(model_directory, file))
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -16,6 +39,6 @@ setup(
     install_requires=[
         'numpy',
         'einops>=0.3',
-        'torch>=1.6',
+        'torch>=1.8',
     ],
 )
