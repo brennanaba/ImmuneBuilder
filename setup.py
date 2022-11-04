d3f3963c6b418570fcf300231bc8a08d2318644e
Brennan Abanades <spamspambrennan@gmail.com>
2022-11-04 15:55:47 +0000

    Add train data

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ImmuneBuilder',
-    version='0.0.2',
+    version='0.0.3',
     description='Set of functions to predict the structure of immune receptor proteins',
     license='BSD 3-clause license',
     maintainer='Brennan Abanades',
